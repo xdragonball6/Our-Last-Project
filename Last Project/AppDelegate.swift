5a842f279c5fd8b2cb6605d2fa9047f24d10ae6a
leeyoonjae <leeyoonjae@leeyoonjaes-MacBook-Air.local>
2023-10-20 14:44:54 +0900

    파이어베이스

--- Last Project/AppDelegate.swift
+++ Last Project/AppDelegate.swift
@@ -8,6 +8,8 @@
 import UIKit
 import KakaoSDKCommon
 import KakaoSDKAuth
+import Firebase
+
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
@@ -16,6 +18,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
+        
+        // Firebase
+        FirebaseApp.configure()
         
         // Kakao SDK 초기화
         let nativeAppKey = Bundle.main.infoDictionary?["KAKAO_NATIVE_APP_KEY"] ?? ""
