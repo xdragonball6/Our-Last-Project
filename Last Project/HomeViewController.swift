a4361456a4773f257526b2b7930aee896e1c76cd
leeyoonjae <leeyoonjae@leeyoonjaes-MacBook-Air.local>
2023-10-26 01:50:24 +0900

    메인 페이지 네비게이션 타이틀 로고 이미지 추가

--- Last Project/HomeViewController.swift
+++ Last Project/HomeViewController.swift
@@ -39,12 +39,39 @@ class HomeViewController: UIViewController, UIScrollViewDelegate{
         startImageAutoScroll()
         downloadImages()
         firebaseMainPageBtn.MainpageImageBtn()
-        
+
         // CollectionView의 데이터 소스와 대리자를 현재 뷰 컨트롤러로 설정
         MainBtnCollectionView.dataSource = self
         MainBtnCollectionView.delegate = self
+
+        // 네비게이션 타이틀 이미지 다운로드 및 설정
+        if let imageURL = URL(string: "https://firebasestorage.googleapis.com/v0/b/lastproject-7fa23.appspot.com/o/mainLogo%2F%E1%84%85%E1%85%A9%E1%84%80%E1%85%A9.png?alt=media&token=1cc1d45c-5af7-43bb-8de4-c88aacd2f03a&_gl=1*1i1b2jk*_ga*MTMxNjcyMzI2LjE2OTE0MjQ4MTU.*_ga_CW55HF8NVT*MTY5ODI1MTQwMS45OC4xLjE2OTgyNTIyMDMuMzguMC4w") {
+            URLSession.shared.dataTask(with: imageURL) { [weak self] (data, response, error) in
+                if let error = error {
+                    print("Error downloading image: \(error)")
+                } else if let data = data, let image = UIImage(data: data) {
+                    // 이미지 다운로드 완료 후 UI 업데이트는 메인 스레드에서 수행해야 합니다.
+                    DispatchQueue.main.async {
+                        // 원격 이미지를 타이틀 뷰로 설정
+                        let imageView = UIImageView(image: image)
+                        imageView.contentMode = .scaleAspectFit
+                        self?.navigationItem.titleView = imageView
+                    }
+                }
+            }.resume()
+        }
     }
 
+
+    
+    @IBAction func reservationBtn(_ sender: UIBarButtonItem) {
+        
+        
+        
+        
+        
+    }
+    
 
     override func viewWillAppear(_ animated: Bool) {
         readValues()
---
+++ Last Project/ViewController/MainPageReservationCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  MainPageReservationCollectionViewCell.swift
+//  Last Project
+//
+//  Created by leeyoonjae on 10/26/23.
+//
+
+import UIKit
+
+class MainPageReservationCollectionViewCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var lblCalendar: UILabel!
+    
+    
+}
